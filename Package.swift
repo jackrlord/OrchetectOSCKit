026fb79ef5eb143b4e1f2816b9c7f04a6e2af1f9
jackrlord <40400539+jackrlord@users.noreply.github.com>
2025-01-30 14:09:41 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "OSCKit",
+    name: "OrchetectOSCKit",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(
-            name: "OSCKit",
+            name: "OrchetectOSCKit",
             targets: ["OSCKit"]
         ),
         .library(
