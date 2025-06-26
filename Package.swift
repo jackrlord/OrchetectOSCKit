278e6343844df2a9c2cd7bc7dbf8c3f1eea8c318
jackrlord <40400539+jackrlord@users.noreply.github.com>
2025-06-26 15:51:44 +0300

    rename updates.

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         ),
         .testTarget(
             name: "OSCKitTests",
-            dependencies: ["OrchetectOOSCKit"]
+            dependencies: ["OrchetectOSCKit"]
         ),
         .testTarget(
             name: "OSCKitCoreTests",
