1f59a262bed60cd51ee518b2c37c55d01f2407d1
jackrlord <40400539+jackrlord@users.noreply.github.com>
2025-06-27 15:57:19 +0300

    names

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .target(
             name: "OrchetectOSCKit",
             dependencies: [
-                "OSCKit",
+                "OrchetectOSCKitCore",
                 .product(
                     name: "CocoaAsyncSocket",
                     package: "CocoaAsyncSocket",
